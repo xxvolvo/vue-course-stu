3bfb8468ea299082fe54d36654c601c64bb166b5
xxvolvo <385789650@qq.com>
2019-01-17 12:35:53 +0800

    dom-2

--- src/components/split-pane/split-pane.vue
+++ src/components/split-pane/split-pane.vue
@@ -1,26 +1,81 @@
 <template>
-  <div class="split-pane-wrapper">
-    <div class="pane pane-left" :style="{width:leftOffSetFromat}"></div>
-    <div></div>
-    <div class="pane pane-right" :style="{left:leftOffSetFromat}"></div>
+  <div class="split-pane-wrapper" ref="outer">
+    <div class="pane pane-left" :style="{width:leftOffSetFromat}">
+      <slot name="left"></slot>
+    </div>
+    <div
+      class="pane-trigger-con"
+      @mousedown="handleMousedown"
+      :style="{left:triggerLeft, width:`${triggerWidth}px`}"
+    ></div>
+    <div class="pane pane-right" :style="{left:leftOffSetFromat}">
+      <slot name="right"></slot>
+    </div>
   </div>
 </template>
 <script>
 export default {
   name: "SplitPane",
+  props: {
+    value: {
+      type: Number,
+      default: 0.8
+    },
+    triggerWidth: {
+      type: Number,
+      default: 8
+    },
+    min: {
+      type: Number,
+      default: 0
+    },
+    max: {
+      type: Number,
+      default: 1
+    }
+  },
   data() {
     return {
-      leftOffSet: 0.3
+      canMove: false,
+      initOffset: 0
     };
   },
   computed: {
     leftOffSetFromat() {
-      return `${this.leftOffSet * 100}%`;
+      return `${this.value * 100}%`;
+    },
+    triggerLeft() {
+      return `calc(${this.value * 100}% - ${this.triggerWidth / 2}px)`;
     }
   },
   methods: {
-    update() {
-      this.leftOffSet += 0.1;
+    handleMousemove(event) {
+      if (!this.canMove) return;
+      const outerRect = this.$refs.outer.getBoundingClientRect();
+      let offsetPercent =
+        (event.pageX -
+          this.initOffset +
+          this.triggerWidth / 2 -
+          outerRect.left) /
+        outerRect.width;
+      if (offsetPercent < this.min) {
+        offsetPercent = this.min;
+      }
+      if (offsetPercent > this.max) {
+        offsetPercent = this.max;
+      }
+      // this.$emit("input", offsetPercent);
+      this.$emit("update:value", offsetPercent);
+    },
+    handleMouseup(event) {
+      this.canMove = false;
+    },
+    handleMousedown(event) {
+      document.addEventListener("mousemove", this.handleMousemove);
+      document.addEventListener("mouseup", this.handleMouseup);
+      this.initOffset =
+        event.pageX - event.srcElement.getBoundingClientRect().left;
+      this.canMove = true;
     }
   }
 };
@@ -33,15 +88,25 @@ export default {
     height: 100%;
     position: absolute;
     top: 0;
+
     &-left {
       background: pink;
     }
+
     &-right {
       background: green;
-      top: 0;
+      right: 0;
       bottom: 0;
+    }
+    &-trigger-con {
+      height: 100%;
+      background: red;
+      position: absolute;
+      top: 0;
+      z-index: 10;
+      user-select: none;
+      cursor: col-resize;
     }
   }
 }
 </style>
-
